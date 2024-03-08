91f9814da0fd310fc1423608529a59a84b896e0d
pratap_51 <maharana1982@gmail.com>
2024-03-08 10:48:01 +0530

    first commit

---
+++ src/test/java/com/example/demojenkinsdocker/DemoJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
